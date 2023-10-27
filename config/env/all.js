6fb2080ea4fe65b15b81b6ef7f7ca76857c7587d
Michael Boeynaems <mboeynaems@outlook.com>
2023-10-27 08:12:38 +0200

    updatesession

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,7 @@
 // default app configuration
 const port = process.env.PORT || 4000;
-let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+//let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+let db = "mongodb://root:oVsGh3X4hAa8RNw@mongodb.portasecura.com:27019/nodegoat?&authSource=admin&retryWrites=true&w=majority"
 
 module.exports = {
     port,
