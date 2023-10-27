46c2b96b4b31ecb3251203617df578f8232ebf77
Mich-b <Mich-b@users.noreply.github.com>
2023-10-27 06:06:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ MongoClient.connect(db, (err, db) => {
         // TODO: Add "maxAge"
         cookie: {
             httpOnly: true,
-            sameSite: 'none'
+            sameSite: 'none',
             // Remember to start an HTTPS server to get this working
             // secure: true
         }
