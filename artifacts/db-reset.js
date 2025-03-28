fee611cac77bf12c02d6de1ca5f431e75cfd9eab
Mich-b <Mich-b@users.noreply.github.com>
2025-03-28 10:19:44 +0100

    add more users

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -808,6 +808,406 @@ const USERS_TO_INSERT = [
         "lastName": "Wilson",
         "benefitStartDate": "2027-01-28",
         "password": "User99_123"
+    },
+    {
+        "_id": 101,
+        "userName": "user101",
+        "firstName": "Jamie",
+        "lastName": "Wilson",
+        "benefitStartDate": "2026-05-24",
+        "password": "User101_123"
+    },
+    {
+        "_id": 102,
+        "userName": "user102",
+        "firstName": "Riley",
+        "lastName": "Martinez",
+        "benefitStartDate": "2026-04-18",
+        "password": "User102_123"
+    },
+    {
+        "_id": 103,
+        "userName": "user103",
+        "firstName": "Jordan",
+        "lastName": "Davis",
+        "benefitStartDate": "2027-06-10",
+        "password": "User103_123"
+    },
+    {
+        "_id": 104,
+        "userName": "user104",
+        "firstName": "Sam",
+        "lastName": "Johnson",
+        "benefitStartDate": "2025-07-13",
+        "password": "User104_123"
+    },
+    {
+        "_id": 105,
+        "userName": "user105",
+        "firstName": "Morgan",
+        "lastName": "Davis",
+        "benefitStartDate": "2026-10-14",
+        "password": "User105_123"
+    },
+    {
+        "_id": 106,
+        "userName": "user106",
+        "firstName": "Alex",
+        "lastName": "Martinez",
+        "benefitStartDate": "2025-11-19",
+        "password": "User106_123"
+    },
+    {
+        "_id": 107,
+        "userName": "user107",
+        "firstName": "Alex",
+        "lastName": "Garcia",
+        "benefitStartDate": "2026-09-10",
+        "password": "User107_123"
+    },
+    {
+        "_id": 108,
+        "userName": "user108",
+        "firstName": "Jamie",
+        "lastName": "Miller",
+        "benefitStartDate": "2025-10-11",
+        "password": "User108_123"
+    },
+    {
+        "_id": 109,
+        "userName": "user109",
+        "firstName": "Taylor",
+        "lastName": "Wilson",
+        "benefitStartDate": "2025-09-16",
+        "password": "User109_123"
+    },
+    {
+        "_id": 110,
+        "userName": "user110",
+        "firstName": "Jordan",
+        "lastName": "Davis",
+        "benefitStartDate": "2025-11-10",
+        "password": "User110_123"
+    },
+    {
+        "_id": 111,
+        "userName": "user111",
+        "firstName": "Taylor",
+        "lastName": "Johnson",
+        "benefitStartDate": "2025-09-17",
+        "password": "User111_123"
+    },
+    {
+        "_id": 112,
+        "userName": "user112",
+        "firstName": "Jamie",
+        "lastName": "Garcia",
+        "benefitStartDate": "2026-10-06",
+        "password": "User112_123"
+    },
+    {
+        "_id": 113,
+        "userName": "user113",
+        "firstName": "Taylor",
+        "lastName": "Johnson",
+        "benefitStartDate": "2026-08-29",
+        "password": "User113_123"
+    },
+    {
+        "_id": 114,
+        "userName": "user114",
+        "firstName": "Taylor",
+        "lastName": "Brown",
+        "benefitStartDate": "2026-10-05",
+        "password": "User114_123"
+    },
+    {
+        "_id": 115,
+        "userName": "user115",
+        "firstName": "Avery",
+        "lastName": "Johnson",
+        "benefitStartDate": "2027-10-10",
+        "password": "User115_123"
+    },
+    {
+        "_id": 116,
+        "userName": "user116",
+        "firstName": "Casey",
+        "lastName": "Johnson",
+        "benefitStartDate": "2025-06-08",
+        "password": "User116_123"
+    },
+    {
+        "_id": 117,
+        "userName": "user117",
+        "firstName": "Riley",
+        "lastName": "Smith",
+        "benefitStartDate": "2026-11-05",
+        "password": "User117_123"
+    },
+    {
+        "_id": 118,
+        "userName": "user118",
+        "firstName": "Jordan",
+        "lastName": "Johnson",
+        "benefitStartDate": "2025-05-15",
+        "password": "User118_123"
+    },
+    {
+        "_id": 119,
+        "userName": "user119",
+        "firstName": "Quinn",
+        "lastName": "Garcia",
+        "benefitStartDate": "2025-04-03",
+        "password": "User119_123"
+    },
+    {
+        "_id": 120,
+        "userName": "user120",
+        "firstName": "Morgan",
+        "lastName": "Brown",
+        "benefitStartDate": "2027-01-08",
+        "password": "User120_123"
+    },
+    {
+        "_id": 121,
+        "userName": "user121",
+        "firstName": "Jordan",
+        "lastName": "Miller",
+        "benefitStartDate": "2027-07-20",
+        "password": "User121_123"
+    },
+    {
+        "_id": 122,
+        "userName": "user122",
+        "firstName": "Morgan",
+        "lastName": "Garcia",
+        "benefitStartDate": "2026-02-10",
+        "password": "User122_123"
+    },
+    {
+        "_id": 123,
+        "userName": "user123",
+        "firstName": "Casey",
+        "lastName": "Davis",
+        "benefitStartDate": "2025-08-11",
+        "password": "User123_123"
+    },
+    {
+        "_id": 124,
+        "userName": "user124",
+        "firstName": "Alex",
+        "lastName": "Lee",
+        "benefitStartDate": "2027-05-30",
+        "password": "User124_123"
+    },
+    {
+        "_id": 125,
+        "userName": "user125",
+        "firstName": "Jamie",
+        "lastName": "Davis",
+        "benefitStartDate": "2026-02-07",
+        "password": "User125_123"
+    },
+    {
+        "_id": 126,
+        "userName": "user126",
+        "firstName": "Avery",
+        "lastName": "Davis",
+        "benefitStartDate": "2027-08-05",
+        "password": "User126_123"
+    },
+    {
+        "_id": 127,
+        "userName": "user127",
+        "firstName": "Jordan",
+        "lastName": "Martinez",
+        "benefitStartDate": "2027-07-14",
+        "password": "User127_123"
+    },
+    {
+        "_id": 128,
+        "userName": "user128",
+        "firstName": "Riley",
+        "lastName": "Garcia",
+        "benefitStartDate": "2026-01-31",
+        "password": "User128_123"
+    },
+    {
+        "_id": 129,
+        "userName": "user129",
+        "firstName": "Jamie",
+        "lastName": "Miller",
+        "benefitStartDate": "2026-10-03",
+        "password": "User129_123"
+    },
+    {
+        "_id": 130,
+        "userName": "user130",
+        "firstName": "Taylor",
+        "lastName": "Davis",
+        "benefitStartDate": "2026-03-29",
+        "password": "User130_123"
+    },
+    {
+        "_id": 131,
+        "userName": "user131",
+        "firstName": "Taylor",
+        "lastName": "Lee",
+        "benefitStartDate": "2026-08-02",
+        "password": "User131_123"
+    },
+    {
+        "_id": 132,
+        "userName": "user132",
+        "firstName": "Avery",
+        "lastName": "Davis",
+        "benefitStartDate": "2027-03-02",
+        "password": "User132_123"
+    },
+    {
+        "_id": 133,
+        "userName": "user133",
+        "firstName": "Avery",
+        "lastName": "Wilson",
+        "benefitStartDate": "2027-06-18",
+        "password": "User133_123"
+    },
+    {
+        "_id": 134,
+        "userName": "user134",
+        "firstName": "Sam",
+        "lastName": "Anderson",
+        "benefitStartDate": "2026-07-01",
+        "password": "User134_123"
+    },
+    {
+        "_id": 135,
+        "userName": "user135",
+        "firstName": "Morgan",
+        "lastName": "Brown",
+        "benefitStartDate": "2025-09-23",
+        "password": "User135_123"
+    },
+    {
+        "_id": 136,
+        "userName": "user136",
+        "firstName": "Jamie",
+        "lastName": "Smith",
+        "benefitStartDate": "2026-09-04",
+        "password": "User136_123"
+    },
+    {
+        "_id": 137,
+        "userName": "user137",
+        "firstName": "Riley",
+        "lastName": "Wilson",
+        "benefitStartDate": "2026-05-08",
+        "password": "User137_123"
+    },
+    {
+        "_id": 138,
+        "userName": "user138",
+        "firstName": "Quinn",
+        "lastName": "Brown",
+        "benefitStartDate": "2027-09-03",
+        "password": "User138_123"
+    },
+    {
+        "_id": 139,
+        "userName": "user139",
+        "firstName": "Avery",
+        "lastName": "Wilson",
+        "benefitStartDate": "2027-12-09",
+        "password": "User139_123"
+    },
+    {
+        "_id": 140,
+        "userName": "user140",
+        "firstName": "Alex",
+        "lastName": "Miller",
+        "benefitStartDate": "2027-03-11",
+        "password": "User140_123"
+    },
+    {
+        "_id": 141,
+        "userName": "user141",
+        "firstName": "Jordan",
+        "lastName": "Wilson",
+        "benefitStartDate": "2026-12-28",
+        "password": "User141_123"
+    },
+    {
+        "_id": 142,
+        "userName": "user142",
+        "firstName": "Jamie",
+        "lastName": "Anderson",
+        "benefitStartDate": "2026-02-27",
+        "password": "User142_123"
+    },
+    {
+        "_id": 143,
+        "userName": "user143",
+        "firstName": "Riley",
+        "lastName": "Davis",
+        "benefitStartDate": "2026-03-24",
+        "password": "User143_123"
+    },
+    {
+        "_id": 144,
+        "userName": "user144",
+        "firstName": "Jordan",
+        "lastName": "Wilson",
+        "benefitStartDate": "2026-07-28",
+        "password": "User144_123"
+    },
+    {
+        "_id": 145,
+        "userName": "user145",
+        "firstName": "Morgan",
+        "lastName": "Smith",
+        "benefitStartDate": "2025-06-12",
+        "password": "User145_123"
+    },
+    {
+        "_id": 146,
+        "userName": "user146",
+        "firstName": "Alex",
+        "lastName": "Wilson",
+        "benefitStartDate": "2027-11-03",
+        "password": "User146_123"
+    },
+    {
+        "_id": 147,
+        "userName": "user147",
+        "firstName": "Jamie",
+        "lastName": "Wilson",
+        "benefitStartDate": "2025-06-02",
+        "password": "User147_123"
+    },
+    {
+        "_id": 148,
+        "userName": "user148",
+        "firstName": "Quinn",
+        "lastName": "Lee",
+        "benefitStartDate": "2026-10-30",
+        "password": "User148_123"
+    },
+    {
+        "_id": 149,
+        "userName": "user149",
+        "firstName": "Jordan",
+        "lastName": "Anderson",
+        "benefitStartDate": "2026-04-27",
+        "password": "User149_123"
+    },
+    {
+        "_id": 150,
+        "userName": "user150",
+        "firstName": "Jordan",
+        "lastName": "Garcia",
+        "benefitStartDate": "2027-11-24",
+        "password": "User150_123"
     }
 ];
     
